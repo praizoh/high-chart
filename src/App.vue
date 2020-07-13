48cde933bbf9c31ebe21460bcc0c4b67fee63da4
praizoh <oremei.akande@gmail.com>
2020-07-13 13:26:56 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-content>
+      <BarChart/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import BarChart from './components/Barchart';
+
+export default {
+  name: 'App',
+
+  components: {
+    BarChart,
+  },
+
+  data(){
+    return{
+
+    }
+  }
+};
+</script>
---
+++ src/views/Bloodgroup.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>
+        <BarChart/>
+    </v-container>    
+</template>
+
+<script>
+import BarChart from './components/Barchart';
+
+export default {
+  name: 'Bloodgroup',
+
+  components: {
+    BarChart,
+  },
+
+  data(){
+    return{
+
+    }
+  }
+};
+</script>
